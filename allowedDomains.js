75baa5b51b42e05fbcfb39514ea97a5b859267ed
JensSudibyo <135489722+JensSudibyo@users.noreply.github.com>
2024-10-28 01:19:44 +0800

    Create allowedDomains.js

---
+++ allowedDomains.js
@@ -0,0 +1,15 @@
+// Daftar domain yang diizinkan
+const allowedDomains = [
+  'https://akusukagratisanlo.blogspot.com/',
+  'https://www.kltraid.online/',
+  'https://bikinbaru96.blogspot.com/'
+];
+
+// Ambil referer dari dokumen
+const referer = document.referrer;
+
+// Cek apakah halaman ini diakses langsung atau dari allowed domain
+if (!referer || !allowedDomains.some(domain => referer.startsWith(domain))) {
+  // Jika tidak ada referer (akses langsung) atau referer tidak sesuai, redirect ke halaman utama
+  window.location.href = 'https://kltraid.pages.dev/';
+}

cbe8aa2d0546556bcef50e5aa77bf9545abb1236
JensSudibyo <135489722+JensSudibyo@users.noreply.github.com>
2024-10-31 07:52:38 +0800

    Update eventData.js

--- eventData.js
+++ eventData.js
@@ -1,5 +1,5 @@
 const eventData = {
-  id: "665f9989-79a4-46d7-8c74-60cbceb07b77",
+  dataId: "665f9989-79a4-46d7-8c74-60cbceb07b77",
   url: "https://yrprmyzif03.yolthklg.xln/k/erwvl-kozbvi-ruiznv.html?pvb=iyxURxSpJi_token=989324ehhedfkhjswf32423kjhksdfgsdge425t34t4e",
   servers: [
     { url: "https://yrprmyzif03.yolthklg.xln/k/erwvl-kozbvi-ruiznv.html?pvb=iyxURxSpJi_token=989324ehhedfkhjswf32423kjhksdfgsdge425t34t4e", label: "Stable" },
@@ -7,22 +7,17 @@ const eventData = {
     { url: "https://ovyl.sfnrmyriw.xm/orev/yzooyzi_79189.m3u8_token=989324ehhedfkhjswf32423kjhksdfgsdge425t34t4e", label: "SD [☑️IOS]" },
     { url: "https://ovyl.sfnrmyriw.xm/orev/yzooyzi_66939.m3u8_token=989324ehhedfkhjswf32423kjhksdfgsdge425t34t4e", label: "SD [☑️IOS]" }
   ],
-  duration: "2.08",
+  eventDuration: "2.08",
   eventName: "Copa Sudamericana",
-  sportIcon: "https://i.imgur.com/oJfBO55.png",
-  team1: {
-    name: "Lanus",
-    logo: "https://ssl.gstatic.com/onebox/media/sports/logos/FiqktuVwEcYAOZNp32H-OQ_96x96.png"
-  },
-  team2: {
-    name: "Cruzeiro",
-    logo: "https://ssl.gstatic.com/onebox/media/sports/logos/Tcv9X__nIh-6wFNJPMwIXQ_96x96.png"
-  },
+  icon: "https://i.imgur.com/oJfBO55.png",
+  logoTeam1: "https://ssl.gstatic.com/onebox/media/sports/logos/FiqktuVwEcYAOZNp32H-OQ_96x96.png",
+  team1Name: "Lanus",
   matchDate: "2024-10-31",
   matchTime: "05:00",
   liveLabel: "Live",
+  logoTeam2: "https://ssl.gstatic.com/onebox/media/sports/logos/Tcv9X__nIh-6wFNJPMwIXQ_96x96.png",
+  team2Name: "Cruzeiro",
   countdown: {
-    title: "Event will start in:",
     timerId: "countdown-665f9989-79a4-46d7-8c74-60cbceb07b77"
   }
 };

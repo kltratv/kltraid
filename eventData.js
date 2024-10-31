33312a3038650d2eb3c105c9e3bdf0dd540e73b3
JensSudibyo <135489722+JensSudibyo@users.noreply.github.com>
2024-11-01 05:41:56 +0800

    Create eventData.js

---
+++ eventData.js
@@ -0,0 +1,23 @@
+const eventData = {
+    dataId: "9da34d58-69aa-4289-b917-73fd26a148d5",
+    dataUrl: "https://yrprmyzif03.yolthklg.xln/k/erwvl-kozbvi-ruiznv.html?pvb=blqHc9t4J1_token=989324ehhedfkhjswf32423kjhksdfgsdge425t34t4e",
+    servers: [
+        { url: "https://yrprmyzif03.yolthklg.xln/k/erwvl-kozbvi-ruiznv.html?pvb=blqHc9t4J1_token=989324ehhedfkhjswf32423kjhksdfgsdge425t34t4e", label: "Stable" },
+        { url: "https://ovyl.sfnrmyriw.xm/orev/yzooyzi_88339.m3u8_token=989324ehhedfkhjswf32423kjhksdfgsdge425t34t4e", label: "SD [☑️IOS]" },
+        { url: "https://ovyl.sfnrmyriw.xm/orev/yzooyzi_69009.m3u8_token=989324ehhedfkhjswf32423kjhksdfgsdge425t34t4e", label: "SD [☑️IOS]" },
+        { url: "https://ovyl.sfnrmyriw.xm/orev/yzooyzi_68619.m3u8_token=989324ehhedfkhjswf32423kjhksdfgsdge425t34t4e", label: "SD [☑️IOS]" }
+    ],
+    sportIcon: "https://i.imgur.com/oJfBO55.png",
+    eventName: "Copa Sudamericana",
+    team1: {
+        logo: "https://ssl.gstatic.com/onebox/media/sports/logos/wi-J-3U7th2bpIB_Uy9Euw_96x96.png",
+        name: "Racing Club"
+    },
+    team2: {
+        logo: "https://ssl.gstatic.com/onebox/media/sports/logos/tCMSqgXVHROpdCpQhzTo1g_96x96.png",
+        name: "Corinthians"
+    },
+    eventDate: "2024-11-01",
+    eventTime: "07:30",
+    duration: 2.08
+};

6eb6bdfe8856b80d610b282579fc228144ae871d
JensSudibyo <135489722+JensSudibyo@users.noreply.github.com>
2024-11-01 14:17:06 +0800

    Update event data

--- eventData.js
+++ eventData.js
@@ -1,76 +1,59 @@
 // BEGIN EVENT DATA
 const eventData = [
-{
-    "dataId": "97458960-3b00-4704-98aa-9a1f4c269628",
-    "dataUrl": "https://kltraid.pages.dev/play.html?channel=ssc2",
-    "servers": [
-        {
-            "url": "https://kltraid.pages.dev/play.html?channel=ssc2",
-            "label": "HD AUTO"
-        },
-        {
-            "url": "https://kltraid.pages.dev/play.html?channel=tnt2",
-            "label": "HD AUTO"
-        },
-        {
-            "url": "https://kltraid.pages.dev/play.html?channel=spoid2",
-            "label": "HD AUTO"
-        },
-        {
-            "url": "https://bikinbaru96.blogspot.com/2024/07/sport7pm-1.html",
-            "label": "HD VN [IOS]"
-        }
-    ],
-    "sportIcon": "https://i.imgur.com/oJfBO55.png",
-    "eventName": "MotoGP Malaysia Grand Prix",
-    "team1": {
-        "logo": "https://upload.wikimedia.org/wikipedia/commons/a/a0/Moto_Gp_logo.svg",
-        "name": "MotoGP"
-    },
-    "team2": {
-        "logo": "https://upload.wikimedia.org/wikipedia/commons/a/a0/Moto_Gp_logo.svg",
-        "name": "MotoGP"
-    },
-    "eventDate": "2024-11-01",
-    "eventTime": "08:00",
-    "duration": "20",
-    "countdownId": "countdown-97458960-3b00-4704-98aa-9a1f4c269628"
-},
-{
-    "dataId": "7ac6955f-e348-4bdd-8bdf-7186c7c05fbd",
-    "dataUrl": "https://kltraid.pages.dev/play.html?channel=ssc2",
-    "servers": [
-        {
-            "url": "https://kltraid.pages.dev/play.html?channel=ssc2",
-            "label": "HD AUTO"
-        },
-        {
-            "url": "https://kltraid.pages.dev/play.html?channel=tnt2",
-            "label": "HD AUTO"
-        },
-        {
-            "url": "https://kltraid.pages.dev/play.html?channel=spoid2",
-            "label": "HD AUTO"
-        },
-        {
-            "url": "https://bikinbaru96.blogspot.com/2024/07/sport7pm-1.html",
-            "label": "HD VN [IOS]"
-        }
-    ],
-    "sportIcon": "https://i.imgur.com/oJfBO55.png",
-    "eventName": "MotoGP Mexico",
-    "team1": {
-        "logo": "https://upload.wikimedia.org/wikipedia/commons/a/a0/Moto_Gp_logo.svg",
-        "name": "MotoGP"
-    },
-    "team2": {
-        "logo": "https://upload.wikimedia.org/wikipedia/commons/a/a0/Moto_Gp_logo.svg",
-        "name": "MotoGP"
-    },
-    "eventDate": "2024-11-01",
-    "eventTime": "08:00",
-    "duration": "20",
-    "countdownId": "countdown-7ac6955f-e348-4bdd-8bdf-7186c7c05fbd"
-}
+<div class="event-container" data-id="6550200c-760a-45bd-9d16-aa9cbc14e39c" data-url="https://pogizrw.kztvh.wve/kozb.html?xszmmvo=hhx7_token=989324ehhedfkhjswf32423kjhksdfgsdge425t34t4e" data-servers='[
+        {"url": "https://pogizrw.kztvh.wve/kozb.html?xszmmvo=hhx7_token=989324ehhedfkhjswf32423kjhksdfgsdge425t34t4e", "label": "HD AUTO"},
+        {"url": "https://pogizrw.kztvh.wve/kozb.html?xszmmvo=gmg7_token=989324ehhedfkhjswf32423kjhksdfgsdge425t34t4e", "label": "HD AUTO"},
+        {"url": "https://pogizrw.kztvh.wve/kozb.html?xszmmvo=hklrw7_token=989324ehhedfkhjswf32423kjhksdfgsdge425t34t4e", "label": "HD AUTO"},
+        {"url": "https://yrprmyzif03.yolthklg.xln/7975/92/hklig2kn-8.html_token=989324ehhedfkhjswf32423kjhksdfgsdge425t34t4e", "label": "HD VN [IOS]"}
+    ]' data-duration="20">
+  <h2><img src="https://i.imgur.com/oJfBO55.png" alt="Sport Icon" class="sport-icon">MotoGP Malaysia Grand Prix</h2>
+  <div class="team">
+    <img src="https://upload.wikimedia.org/wikipedia/commons/a/a0/Moto_Gp_logo.svg" alt="MotoGP" class="team-logo">
+    <span>MotoGP</span>
+  </div>
+  <div class="match-date">2024-11-01</div>
+  <div class="match-time">08:00</div>
+  <div class="live-label" style="display:none;">Live</div>
+  <div class="team">
+    <img src="https://upload.wikimedia.org/wikipedia/commons/a/a0/Moto_Gp_logo.svg" alt="MotoGP" class="team-logo">
+    <span>MotoGP</span>
+  </div>
+  <div class="server-buttons" style="display:none;">
+    <div class="instruction">You can select a server stream:</div>
+    <div class="buttons-container"></div>
+  </div>
+  <div class="countdown-wrapper" id="countdown-6550200c-760a-45bd-9d16-aa9cbc14e39c" style="display:none;">
+      <div class="countdown-title">Event will start in:</div>
+      <div class="countdown-timer"></div>
+  </div>
+</div>,
+<div class="event-container" data-id="02b7f814-bf51-40f7-9c84-4f0785081478" data-url="https://ovyl.sfnrmyriw.xm/orev/yzooyzi_69859.m3u8_token=989324ehhedfkhjswf32423kjhksdfgsdge425t34t4e" data-servers='[
+        {"url": "https://ovyl.sfnrmyriw.xm/orev/yzooyzi_69859.m3u8_token=989324ehhedfkhjswf32423kjhksdfgsdge425t34t4e", "label": "COURT 1"},
+        {"url": "https://ovyl.sfnrmyriw.xm/orev/yzooyzi_71819.m3u8_token=989324ehhedfkhjswf32423kjhksdfgsdge425t34t4e", "label": "COURT 2"},
+        {"url": "https://ovyl.sfnrmyriw.xm/orev/yzooyzi_76249.m3u8_token=989324ehhedfkhjswf32423kjhksdfgsdge425t34t4e", "label": "COURT 3"},
+        {"url": "https://ovyl.sfnrmyriw.xm/orev/yzooyzi_77809.m3u8_token=989324ehhedfkhjswf32423kjhksdfgsdge425t34t4e", "label": "COURT 4"},
+        {"url": "https://ovyl.sfnrmyriw.xm/orev/yzooyzi_71119.m3u8_token=989324ehhedfkhjswf32423kjhksdfgsdge425t34t4e", "label": "COURT 5"}
+    ]' data-duration="20">
+  <h2><img src="https://i.imgur.com/F597ABm.png" alt="Sport Icon" class="sport-icon">BWF Tour Indonesia Masters</h2>
+  <div class="team">
+    <img src="https://i.imgur.com/F597ABm.png" alt="Badminton" class="team-logo">
+    <span>Badminton</span>
+  </div>
+  <div class="match-date">2024-11-01</div>
+  <div class="match-time">13:30</div>
+  <div class="live-label" style="display:none;">Live</div>
+  <div class="team">
+    <img src="https://i.imgur.com/F597ABm.png" alt="Badminton" class="team-logo">
+    <span>Badminton</span>
+  </div>
+  <div class="server-buttons" style="display:none;">
+    <div class="instruction">You can select a server stream:</div>
+    <div class="buttons-container"></div>
+  </div>
+  <div class="countdown-wrapper" id="countdown-02b7f814-bf51-40f7-9c84-4f0785081478" style="display:none;">
+      <div class="countdown-title">Event will start in:</div>
+      <div class="countdown-timer"></div>
+  </div>
+</div>
 ];
 // END EVENT DATA

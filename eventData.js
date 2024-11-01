2aec8c23bfbb8415db9b0d429a8ede85fce0cde9
JensSudibyo <135489722+JensSudibyo@users.noreply.github.com>
2024-11-01 10:36:29 +0800

    Update event data

--- eventData.js
+++ eventData.js
@@ -1,6 +1,6 @@
 // BEGIN EVENT DATA
 const eventData = {
-    "wzgzIw": "5x33860z-6ww8-5005-yv92-4u85wy0u1035",
+    "wzgzIw": "7zy3uw05-u42y-56xx-z359-11x7zu7yv722",
     "wzgzUio": "sggkh://pogizrw.kztvh.wve/kozb.sgno?xszmmvo=hhx7",
     "hvievih": [
         {
@@ -33,6 +33,43 @@ const eventData = {
     "vevmgDzgv": "7975-88-98",
     "vevmgTrnv": "91:99",
     "wfizgrlm": "79",
-    "xlfmgwldmIw": "xlfmgwldm-5x33860z-6ww8-5005-yv92-4u85wy0u1035"
+    "xlfmgwldmIw": "xlfmgwldm-7zy3uw05-u42y-56xx-z359-11x7zu7yv722"
+};
+
+const eventData = {
+    "wzgzIw": "4zv46w2z-xy1x-547y-1u21-9956z54152y5",
+    "wzgzUio": "sggkh://pogizrw.kztvh.wve/kozb.sgno?xszmmvo=hhx7",
+    "hvievih": [
+        {
+            "fio": "sggkh://pogizrw.kztvh.wve/kozb.sgno?xszmmvo=hhx7",
+            "ozyvo": "HD Afgl"
+        },
+        {
+            "fio": "sggkh://pogizrw.kztvh.wve/kozb.sgno?xszmmvo=gmg7",
+            "ozyvo": "HD Afgl"
+        },
+        {
+            "fio": "sggkh://pogizrw.kztvh.wve/kozb.sgno?xszmmvo=hklrw7",
+            "ozyvo": "HD Afgl"
+        },
+        {
+            "fio": "sggkh://yrprmyzif03.yolthklg.xln/7975/92/hklig2kn-8.sgno",
+            "ozyvo": "HD VN [??IOS]"
+        }
+    ],
+    "hkligIxlm": "sggkh://r.rntfi.xln/lJuBO44.kmt",
+    "vevmgNznv": "MlglGP Mvcrxl",
+    "gvzn8": {
+        "oltl": "sggkh://fkolzw.drprnvwrz.lit/drprkvwrz/xlnnlmh/z/z9/Mlgl_Gk_oltl.het",
+        "mznv": "MlglGP"
+    },
+    "gvzn7": {
+        "oltl": "sggkh://fkolzw.drprnvwrz.lit/drprkvwrz/xlnnlmh/z/z9/Mlgl_Gk_oltl.het",
+        "mznv": "MlglGP"
+    },
+    "vevmgDzgv": "7975-88-98",
+    "vevmgTrnv": "91:99",
+    "wfizgrlm": "79",
+    "xlfmgwldmIw": "xlfmgwldm-4zv46w2z-xy1x-547y-1u21-9956z54152y5"
 };
 // END EVENT DATA

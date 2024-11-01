cca1e67fefbe39d785047f1a69b75ede7744ec76
JensSudibyo <135489722+JensSudibyo@users.noreply.github.com>
2024-11-01 14:21:12 +0800

    Update event data

--- eventData.js
+++ eventData.js
@@ -1,33 +1,6 @@
 // BEGIN EVENT DATA
 const eventData = [
-<div class="event-container" data-id="6eee36ce-bf50-47de-82d5-66ce66478365" data-url="https://pogizrw.kztvh.wve/kozb.html?xszmmvo=hhx7_token=989324ehhedfkhjswf32423kjhksdfgsdge425t34t4e" data-servers='[
-        {"url": "https://pogizrw.kztvh.wve/kozb.html?xszmmvo=hhx7_token=989324ehhedfkhjswf32423kjhksdfgsdge425t34t4e", "label": "HD AUTO"},
-        {"url": "https://pogizrw.kztvh.wve/kozb.html?xszmmvo=gmg7_token=989324ehhedfkhjswf32423kjhksdfgsdge425t34t4e", "label": "HD AUTO"},
-        {"url": "https://pogizrw.kztvh.wve/kozb.html?xszmmvo=hklrw7_token=989324ehhedfkhjswf32423kjhksdfgsdge425t34t4e", "label": "HD AUTO"},
-        {"url": "https://yrprmyzif03.yolthklg.xln/7975/92/hklig2kn-8.html_token=989324ehhedfkhjswf32423kjhksdfgsdge425t34t4e", "label": "HD VN [IOS]"}
-    ]' data-duration="20">
-  <h2><img src="https://i.imgur.com/oJfBO55.png" alt="Sport Icon" class="sport-icon">MotoGP Malaysia Grand Prix</h2>
-  <div class="team">
-    <img src="https://upload.wikimedia.org/wikipedia/commons/a/a0/Moto_Gp_logo.svg" alt="MotoGP" class="team-logo">
-    <span>MotoGP</span>
-  </div>
-  <div class="match-date">2024-11-01</div>
-  <div class="match-time">08:00</div>
-  <div class="live-label" style="display:none;">Live</div>
-  <div class="team">
-    <img src="https://upload.wikimedia.org/wikipedia/commons/a/a0/Moto_Gp_logo.svg" alt="MotoGP" class="team-logo">
-    <span>MotoGP</span>
-  </div>
-  <div class="server-buttons" style="display:none;">
-    <div class="instruction">You can select a server stream:</div>
-    <div class="buttons-container"></div>
-  </div>
-  <div class="countdown-wrapper" id="countdown-6eee36ce-bf50-47de-82d5-66ce66478365" style="display:none;">
-      <div class="countdown-title">Event will start in:</div>
-      <div class="countdown-timer"></div>
-  </div>
-</div>,
-<div class="event-container" data-id="47943b82-8720-4457-9425-d366472bc91a" data-url="https://ovyl.sfnrmyriw.xm/orev/yzooyzi_69859.m3u8_token=989324ehhedfkhjswf32423kjhksdfgsdge425t34t4e" data-servers='[
+<div class="event-container" data-id="6f639f82-54c8-4de7-8445-b63a59112065" data-url="https://ovyl.sfnrmyriw.xm/orev/yzooyzi_69859.m3u8_token=989324ehhedfkhjswf32423kjhksdfgsdge425t34t4e" data-servers='[
         {"url": "https://ovyl.sfnrmyriw.xm/orev/yzooyzi_69859.m3u8_token=989324ehhedfkhjswf32423kjhksdfgsdge425t34t4e", "label": "COURT 1"},
         {"url": "https://ovyl.sfnrmyriw.xm/orev/yzooyzi_71819.m3u8_token=989324ehhedfkhjswf32423kjhksdfgsdge425t34t4e", "label": "COURT 2"},
         {"url": "https://ovyl.sfnrmyriw.xm/orev/yzooyzi_76249.m3u8_token=989324ehhedfkhjswf32423kjhksdfgsdge425t34t4e", "label": "COURT 3"},
@@ -50,7 +23,7 @@ const eventData = [
     <div class="instruction">You can select a server stream:</div>
     <div class="buttons-container"></div>
   </div>
-  <div class="countdown-wrapper" id="countdown-47943b82-8720-4457-9425-d366472bc91a" style="display:none;">
+  <div class="countdown-wrapper" id="countdown-6f639f82-54c8-4de7-8445-b63a59112065" style="display:none;">
       <div class="countdown-title">Event will start in:</div>
       <div class="countdown-timer"></div>
   </div>

9fc4273b952215402a1f161f0fee928f3ef58858
JensSudibyo <135489722+JensSudibyo@users.noreply.github.com>
2024-11-02 12:38:59 +0800

    Update event data

--- eventData.js
+++ eventData.js
@@ -1,3 +1,40 @@
 // BEGIN EVENT DATA
-undefined
+const eventData = [
+const eventData = {
+    "dataId": "bee271f2-bc5a-4335-a333-a03a8143c9c0",
+    "dataUrl": "https://www.zhibo1.cc/live_2_8_40810.html",
+    "servers": [
+        {
+            "url": "https://www.zhibo1.cc/live_2_8_40810.html",
+            "label": "COURT 1"
+        },
+        {
+            "url": "https://www.zhibo1.cc/live_2_8_40810.html",
+            "label": "COURT 2"
+        },
+        {
+            "url": "https://www.zhibo1.cc/live_2_8_40810.html",
+            "label": "COURT 3"
+        },
+        {
+            "url": "https://www.zhibo1.cc/live_2_8_40810.html",
+            "label": "COURT 4"
+        }
+    ],
+    "sportIcon": "https://i.imgur.com/F597ABm.png",
+    "eventName": "BWF Tour Indonesia Masters",
+    "team1": {
+        "logo": "https://i.imgur.com/F597ABm.png",
+        "name": "Badminton"
+    },
+    "team2": {
+        "logo": "https://i.imgur.com/F597ABm.png",
+        "name": "Badminton"
+    },
+    "eventDate": "2024-11-01",
+    "eventTime": "13:30",
+    "duration": "20",
+    "countdownId": "countdown-bee271f2-bc5a-4335-a333-a03a8143c9c0"
+};
+];
 // END EVENT DATA

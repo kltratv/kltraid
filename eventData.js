0689ef72e61d8f5b2da57001a0dbbf309772e42e
JensSudibyo <135489722+JensSudibyo@users.noreply.github.com>
2024-11-02 12:36:40 +0800

    Update event data

--- eventData.js
+++ eventData.js
@@ -1,36 +1,3 @@
 // BEGIN EVENT DATA
-const eventData = [
-{
-    "wzgzIw": "z0u3y561-69wv-53y6-1600-350331u9z69z",
-    "wzgzUio": "sggkh://ovyl.sfnrmyriw.xm/orev/yzooyzi_60429.n6f1",
-    "hvievih": [
-        {
-            "fio": "sggkh://ovyl.sfnrmyriw.xm/orev/yzooyzi_60429.n6f1",
-            "ozyvo": "STABLE"
-        },
-        {
-            "fio": "sggkh://ovyl.sfnrmyriw.xm/orev/yzooyzi_81819.n6f1",
-            "ozyvo": "SD [IOS]"
-        },
-        {
-            "fio": "sggkh://ovyl.sfnrmyriw.xm/orev/yzooyzi_2129.n6f1",
-            "ozyvo": "SD [IOS]"
-        }
-    ],
-    "hkligIxlm": "sggkh://r.rntfi.xln/lJuBO44.kmt",
-    "vevmgNznv": "Lrtfv 8",
-    "gvzn8": {
-        "oltl": "sggkh://hho.thgzgrx.xln/lmvylc/nvwrz/hkligh/oltlh/D7AQv1jlbPIP5K1MaLedfA_03c03.kmt",
-        "mznv": "Lroov"
-    },
-    "gvzn7": {
-        "oltl": "sggkh://hho.thgzgrx.xln/lmvylc/nvwrz/hkligh/oltlh/SiKK44wUpCcv5nJhbhsuCt_03c03.kmt",
-        "mznv": "Lblm"
-    },
-    "vevmgDzgv": "7975-88-97",
-    "vevmgTrnv": "96:99",
-    "wfizgrlm": "7.91",
-    "xlfmgwldmIw": "xlfmgwldm-z0u3y561-69wv-53y6-1600-350331u9z69z"
-}
-];
+undefined
 // END EVENT DATA

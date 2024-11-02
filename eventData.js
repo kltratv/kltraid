cb842dd8222f661785d18aa82b7c28175bdcaa2e
JensSudibyo <135489722+JensSudibyo@users.noreply.github.com>
2024-11-02 14:19:46 +0800

    Update eventData.js

--- eventData.js
+++ eventData.js
@@ -1,32 +1,3 @@
 // BEGIN EVENT DATA
-const eventData = [
-{
-    "wzgzIw": "0u7w0u5u-1613-5w5z-1vux-4y9336u50w3y",
-    "wzgzUio": "sggkh://ovyl.sfnrmyriw.xm/orev/yzooyzi_79309.n6f1",
-    "hvievih": [
-        {
-            "fio": "sggkh://ovyl.sfnrmyriw.xm/orev/yzooyzi_79309.n6f1",
-            "ozyvo": "COURT 8"
-        },
-        {
-            "fio": "sggkh://ovyl.sfnrmyriw.xm/orev/yzooyzi_75979.n6f1",
-            "ozyvo": "COURT 7"
-        }
-    ],
-    "hkligIxlm": "sggkh://r.rntfi.xln/F402ABn.kmt",
-    "vevmgNznv": "BWF Tlfi Imwlmvhrz Mzhgvih",
-    "gvzn8": {
-        "oltl": "sggkh://r.rntfi.xln/F402ABn.kmt",
-        "mznv": "Bzwnrmglm"
-    },
-    "gvzn7": {
-        "oltl": "sggkh://r.rntfi.xln/F402ABn.kmt",
-        "mznv": "Bzwnrmglm"
-    },
-    "vevmgDzgv": "7975-88-97",
-    "vevmgTrnv": "87:99",
-    "wfizgrlm": "79",
-    "xlfmgwldmIw": "xlfmgwldm-0u7w0u5u-1613-5w5z-1vux-4y9336u50w3y"
-}
-];
+
 // END EVENT DATA

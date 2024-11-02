9e0b0e76326117e1c995956e9604cf860487619f
JensSudibyo <135489722+JensSudibyo@users.noreply.github.com>
2024-11-02 14:38:53 +0800

    Update event data

--- eventData.js
+++ eventData.js
@@ -1,68 +1,40 @@
 // BEGIN EVENT DATA
 const eventData = [
 {
-    "dataId": "631f7ee1-a2d9-490e-97b4-7b501b6b290d",
-    "dataUrl": "https://jancok.elutuna.workers.dev/f6alM3tx_8zHENxCrKdx8w/Fv2lucGIbhPi2caWYvF5Lg/1730529450301/live/may61/playlist.m3u8",
-    "servers": [
-        {
-            "url": "https://jancok.elutuna.workers.dev/f6alM3tx_8zHENxCrKdx8w/Fv2lucGIbhPi2caWYvF5Lg/1730529450301/live/may61/playlist.m3u8",
-            "label": "HD VN [IOS]"
-        },
-        {
-            "url": "https://kltraid.pages.dev/play.html?channel=ssc2",
-            "label": "HD Auto"
-        },
-        {
-            "url": "https://kltraid.pages.dev/play.html?channel=tnt2",
-            "label": "HD Auto"
-        },
-        {
-            "url": "https://kltraid.pages.dev/play.html?channel=spoid2",
-            "label": "HD Auto"
-        }
-    ],
-    "sportIcon": "https://i.imgur.com/MAla4OO.png",
-    "eventName": "MotoGP Malaysia Grand Prix",
-    "team1": {
-        "logo": "https://upload.wikimedia.org/wikipedia/commons/a/a0/Moto_Gp_logo.svg",
-        "name": "MotoGP"
-    },
-    "team2": {
-        "logo": "https://upload.wikimedia.org/wikipedia/commons/a/a0/Moto_Gp_logo.svg",
-        "name": "MotoGP"
-    },
-    "eventDate": "2024-11-02",
-    "eventTime": "07:40",
-    "duration": "20",
-    "countdownId": "countdown-631f7ee1-a2d9-490e-97b4-7b501b6b290d"
-},
-{
-    "wzgzIw": "vzy18323-v5wu-5966-z407-u3y48208zv59",
-    "wzgzUio": "sggkh://ovyl.sfnrmyriw.xm/orev/yzooyzi_79309.n6f1",
+    "wzgzIw": "63wx1z4v-311z-5w4u-yuz0-9y4v5900v9uu",
+    "wzgzUio": "sggkh://qzmxlp.vofgfmz.dlipvih.wve/u3zoM6gc_1aHENcCiKwc1d/Fe7ofxGIysPr7xzWYeF4Lt/8269470549698/orev/nzb38/kozborhg.n6f1",
     "hvievih": [
         {
-            "fio": "sggkh://ovyl.sfnrmyriw.xm/orev/yzooyzi_79309.n6f1",
-            "ozyvo": "COURT 8"
+            "fio": "sggkh://qzmxlp.vofgfmz.dlipvih.wve/u3zoM6gc_1aHENcCiKwc1d/Fe7ofxGIysPr7xzWYeF4Lt/8269470549698/orev/nzb38/kozborhg.n6f1",
+            "ozyvo": "HD VN [IOS]"
         },
         {
-            "fio": "sggkh://ovyl.sfnrmyriw.xm/orev/yzooyzi_75979.n6f1",
-            "ozyvo": "COURT 7"
+            "fio": "sggkh://pogizrw.kztvh.wve/kozb.sgno?xszmmvo=hhx7",
+            "ozyvo": "HD Afgl"
+        },
+        {
+            "fio": "sggkh://pogizrw.kztvh.wve/kozb.sgno?xszmmvo=gmg7",
+            "ozyvo": "HD Afgl"
+        },
+        {
+            "fio": "sggkh://pogizrw.kztvh.wve/kozb.sgno?xszmmvo=hklrw7",
+            "ozyvo": "HD Afgl"
         }
     ],
-    "hkligIxlm": "sggkh://r.rntfi.xln/F402ABn.kmt",
-    "vevmgNznv": "BWF Tlfi Imwlmvhrz Mzhgvih",
+    "hkligIxlm": "sggkh://r.rntfi.xln/MAoz5OO.kmt",
+    "vevmgNznv": "MlglGP Mzozbhrz Gizmw Pirc",
     "gvzn8": {
-        "oltl": "sggkh://r.rntfi.xln/F402ABn.kmt",
-        "mznv": "Bzwnrmglm"
+        "oltl": "sggkh://fkolzw.drprnvwrz.lit/drprkvwrz/xlnnlmh/z/z9/Mlgl_Gk_oltl.het",
+        "mznv": "MlglGP"
     },
     "gvzn7": {
-        "oltl": "sggkh://r.rntfi.xln/F402ABn.kmt",
-        "mznv": "Bzwnrmglm"
+        "oltl": "sggkh://fkolzw.drprnvwrz.lit/drprkvwrz/xlnnlmh/z/z9/Mlgl_Gk_oltl.het",
+        "mznv": "MlglGP"
     },
     "vevmgDzgv": "7975-88-97",
-    "vevmgTrnv": "87:99",
+    "vevmgTrnv": "92:59",
     "wfizgrlm": "79",
-    "xlfmgwldmIw": "xlfmgwldm-vzy18323-v5wu-5966-z407-u3y48208zv59"
+    "xlfmgwldmIw": "xlfmgwldm-63wx1z4v-311z-5w4u-yuz0-9y4v5900v9uu"
 }
 ];
 // END EVENT DATA

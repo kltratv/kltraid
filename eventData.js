a836b37f1b9c9e9a71da1144cab03e282a6d49b7
JensSudibyo <135489722+JensSudibyo@users.noreply.github.com>
2024-11-02 13:16:14 +0800

    Update event data

--- eventData.js
+++ eventData.js
@@ -1,3 +1,60 @@
 // BEGIN EVENT DATA
-undefined
+const eventData = [
+{
+    "dataId": "8b618bee-e328-4872-b4e8-ad86a617d7dd",
+    "dataUrl": "https://www.zhibo1.cc/live_2_8_40810.html",
+    "servers": [
+        {
+            "url": "https://www.zhibo1.cc/live_2_8_40810.html",
+            "label": "COURT 1"
+        },
+        {
+            "url": "https://www.zhibo1.cc/live_2_8_40810.html",
+            "label": "COURT 2"
+        }
+    ],
+    "sportIcon": "https://i.imgur.com/F597ABm.png",
+    "eventName": "BWF Tour Indonesia Masters",
+    "team1": {
+        "logo": "https://i.imgur.com/F597ABm.png",
+        "name": "Badminton"
+    },
+    "team2": {
+        "logo": "https://i.imgur.com/F597ABm.png",
+        "name": "Badminton"
+    },
+    "eventDate": "2024-11-01",
+    "eventTime": "13:30",
+    "duration": "20",
+    "countdownId": "countdown-8b618bee-e328-4872-b4e8-ad86a617d7dd"
+},
+{
+    "dataId": "456e3eaa-7c3a-4d54-8ef8-993d03725589",
+    "dataUrl": "https://www.zhibo1.cc/live_2_8_40810.html",
+    "servers": [
+        {
+            "url": "https://www.zhibo1.cc/live_2_8_40810.html",
+            "label": "COURT 3"
+        },
+        {
+            "url": "https://www.zhibo1.cc/live_2_8_40810.html",
+            "label": "COURT 4"
+        }
+    ],
+    "sportIcon": "https://i.imgur.com/F597ABm.png",
+    "eventName": "BWF Tour Mexico Masters",
+    "team1": {
+        "logo": "https://i.imgur.com/F597ABm.png",
+        "name": "Badminton"
+    },
+    "team2": {
+        "logo": "https://i.imgur.com/F597ABm.png",
+        "name": "Badminton"
+    },
+    "eventDate": "2024-11-01",
+    "eventTime": "13:30",
+    "duration": "20",
+    "countdownId": "countdown-456e3eaa-7c3a-4d54-8ef8-993d03725589"
+}
+];
 // END EVENT DATA

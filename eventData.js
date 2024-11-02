aae5a2b65575be476d7166bab509cb69ca4f56ef
JensSudibyo <135489722+JensSudibyo@users.noreply.github.com>
2024-11-02 12:50:12 +0800

    Update event data

--- eventData.js
+++ eventData.js
@@ -1,32 +1,32 @@
 // BEGIN EVENT DATA
 const eventData = [
 {
-    "dataId": "3c86601a-66b1-42cc-ad38-bbf0a9a46f82",
-    "dataUrl": "https://www.zhibo1.cc/live_2_8_40810.html",
-    "servers": [
+    "wzgzIw": "6x13398z-33y8-57xx-zw61-yyu9z0z53u17",
+    "wzgzUio": "sggkh://ddd.asryl8.xx/orev_7_1_59189.sgno",
+    "hvievih": [
         {
-            "url": "https://www.zhibo1.cc/live_2_8_40810.html",
-            "label": "COURT 1"
+            "fio": "sggkh://ddd.asryl8.xx/orev_7_1_59189.sgno",
+            "ozyvo": "COURT 8"
         },
         {
-            "url": "https://www.zhibo1.cc/live_2_8_40810.html",
-            "label": "COURT 2"
+            "fio": "sggkh://ddd.asryl8.xx/orev_7_1_59189.sgno",
+            "ozyvo": "COURT 7"
         }
     ],
-    "sportIcon": "https://i.imgur.com/F597ABm.png",
-    "eventName": "BWF Tour Indonesia Masters",
-    "team1": {
-        "logo": "https://i.imgur.com/F597ABm.png",
-        "name": "Badminton"
+    "hkligIxlm": "sggkh://r.rntfi.xln/F402ABn.kmt",
+    "vevmgNznv": "BWF Tlfi Imwlmvhrz Mzhgvih",
+    "gvzn8": {
+        "oltl": "sggkh://r.rntfi.xln/F402ABn.kmt",
+        "mznv": "Bzwnrmglm"
     },
-    "team2": {
-        "logo": "https://i.imgur.com/F597ABm.png",
-        "name": "Badminton"
+    "gvzn7": {
+        "oltl": "sggkh://r.rntfi.xln/F402ABn.kmt",
+        "mznv": "Bzwnrmglm"
     },
-    "eventDate": "2024-11-01",
-    "eventTime": "13:30",
-    "duration": "20",
-    "countdownId": "countdown-3c86601a-66b1-42cc-ad38-bbf0a9a46f82"
+    "vevmgDzgv": "7975-88-98",
+    "vevmgTrnv": "86:69",
+    "wfizgrlm": "79",
+    "xlfmgwldmIw": "xlfmgwldm-6x13398z-33y8-57xx-zw61-yyu9z0z53u17"
 },
 {
     "dataId": "1cdb29ad-3ed3-443d-8b84-003eb4b0ff01",

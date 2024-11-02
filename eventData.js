2844506e811bea2ddeefd480017d481176a627fb
JensSudibyo <135489722+JensSudibyo@users.noreply.github.com>
2024-11-02 10:41:29 +0800

    Update event data

--- eventData.js
+++ eventData.js
@@ -1,7 +1,7 @@
 // BEGIN EVENT DATA
 const eventData = [
 xlmhg vevmgDzgz = {
-    "wzgzIw": "w46yu3x4-63x4-5v77-z468-9823xzuu7451",
+    "wzgzIw": "x51x1867-8z56-51xz-078v-05u02v9x6v94",
     "wzgzUio": "sggkh://ddd.asryl8.xx/orev_7_1_59189.sgno",
     "hvievih": [
         {
@@ -34,7 +34,7 @@ xlmhg vevmgDzgz = {
     "vevmgDzgv": "7975-88-98",
     "vevmgTrnv": "86:69",
     "wfizgrlm": "79",
-    "xlfmgwldmIw": "xlfmgwldm-w46yu3x4-63x4-5v77-z468-9823xzuu7451"
+    "xlfmgwldmIw": "xlfmgwldm-x51x1867-8z56-51xz-078v-05u02v9x6v94"
 }
 ];
 // END EVENT DATA

7ea84067ae338f3f1af02eeb5953f80e3e1ff090
JensSudibyo <135489722+JensSudibyo@users.noreply.github.com>
2024-11-03 20:02:06 +0800

    Update event data

--- eventData.js
+++ eventData.js
@@ -307,6 +307,118 @@ const eventData = [
     "vevmgTrnv": "79:99",
     "wfizgrlm": "7.94",
     "xlfmgwldmIw": "xlfmgwldm-6w9w2269-z50z-5z1v-0y5y-v0u5vw132y7x"
+},
+{
+    "wzgzIw": "w57vxwv4-0730-5899-z218-u509x3u940z3",
+    "wzgzUio": "sggkh://yrprmyzif03.yolthklg.xln/k/erwvl-kozbvi-ruiznv.sgno?pvb=Wku5inXuHk",
+    "hvievih": [
+        {
+            "fio": "sggkh://yrprmyzif03.yolthklg.xln/k/erwvl-kozbvi-ruiznv.sgno?pvb=Wku5inXuHk",
+            "ozyvo": "STABLE"
+        },
+        {
+            "fio": "sggkh://ovyl.sfnrmyriw.xm/orev/yzooyzi_65309.n6f1",
+            "ozyvo": "SD [IOS]"
+        }
+    ],
+    "hkligIxlm": "sggkh://r.rntfi.xln/lJuBO44.kmt",
+    "vevmgNznv": "Tfiprhs Sfkvi Lvztfv",
+    "gvzn8": {
+        "oltl": "sggkh://gnhho.zpznzravw.mvg/rnztvh/dzkkvm/svzw/55993.kmt?on=8558061402",
+        "mznv": "Blwifn"
+    },
+    "gvzn7": {
+        "oltl": "sggkh://hho.thgzgrx.xln/lmvylc/nvwrz/hkligh/oltlh/nw0O9STiEvOCih2gcX5hMA_03c03.kmt",
+        "mznv": "Aozmbzhkli"
+    },
+    "vevmgDzgv": "7975-88-96",
+    "vevmgTrnv": "79:99",
+    "wfizgrlm": "7.94",
+    "xlfmgwldmIw": "xlfmgwldm-w57vxwv4-0730-5899-z218-u509x3u940z3"
+},
+{
+    "wzgzIw": "6874wvwu-0623-5585-0816-97zz83x5zx56",
+    "wzgzUio": "sggkh://yrprmyzif03.yolthklg.xln/k/erwvl-kozbvi-ruiznv.sgno?pvb=irCsO9iDZD",
+    "hvievih": [
+        {
+            "fio": "sggkh://yrprmyzif03.yolthklg.xln/k/erwvl-kozbvi-ruiznv.sgno?pvb=irCsO9iDZD",
+            "ozyvo": "STABLE"
+        },
+        {
+            "fio": "sggkh://ovyl.sfnrmyriw.xm/orev/yzooyzi_75459.n6f1",
+            "ozyvo": "SD [IOS]"
+        }
+    ],
+    "hkligIxlm": "sggkh://r.rntfi.xln/lJuBO44.kmt",
+    "vevmgNznv": "Tfiprhs Sfkvi Lvztfv",
+    "gvzn8": {
+        "oltl": "sggkh://gnhho.zpznzravw.mvg/rnztvh/dzkkvm/svzw/2839.kmt?on=8558183785",
+        "mznv": "Ebfkhkli"
+    },
+    "gvzn7": {
+        "oltl": "sggkh://gnhho.zpznzravw.mvg/rnztvh/dzkkvm/svzw/2224.kmt?on=8558276393",
+        "mznv": "Hzgzbhkli"
+    },
+    "vevmgDzgv": "7975-88-96",
+    "vevmgTrnv": "79:99",
+    "wfizgrlm": "7.94",
+    "xlfmgwldmIw": "xlfmgwldm-6874wvwu-0623-5585-0816-97zz83x5zx56"
+},
+{
+    "wzgzIw": "212z41u8-1y01-599y-1639-7344yy70z4yw",
+    "wzgzUio": "sggkh://ovyl.sfnrmyriw.xm/orev/yzooyzi_74629.n6f1",
+    "hvievih": [
+        {
+            "fio": "sggkh://ovyl.sfnrmyriw.xm/orev/yzooyzi_74629.n6f1",
+            "ozyvo": "COURT 8"
+        }
+    ],
+    "hkligIxlm": "sggkh://r.rntfi.xln/F402ABn.kmt",
+    "vevmgNznv": "BWF Tlfi Hbol Okvm",
+    "gvzn8": {
+        "oltl": "sggkh://r.rntfi.xln/F402ABn.kmt",
+        "mznv": "Bzwnrmglm"
+    },
+    "gvzn7": {
+        "oltl": "sggkh://r.rntfi.xln/F402ABn.kmt",
+        "mznv": "Bzwnrmglm"
+    },
+    "vevmgDzgv": "7975-88-96",
+    "vevmgTrnv": "79:99",
+    "wfizgrlm": "79",
+    "xlfmgwldmIw": "xlfmgwldm-212z41u8-1y01-599y-1639-7344yy70z4yw"
+},
+{
+    "wzgzIw": "2u13x9x1-7x3y-58u5-0486-0x7z1xx3u8z6",
+    "wzgzUio": "sggkh://yrprmyzif03.yolthklg.xln/k/erwvl-kozbvi-ruiznv.sgno?pvb=QEsdDe11FM",
+    "hvievih": [
+        {
+            "fio": "sggkh://yrprmyzif03.yolthklg.xln/k/erwvl-kozbvi-ruiznv.sgno?pvb=QEsdDe11FM",
+            "ozyvo": "STABLE"
+        },
+        {
+            "fio": "sggkh://ovyl.sfnrmyriw.xm/orev/yzooyzi_71029.n6f1",
+            "ozyvo": "SD [IOS]"
+        },
+        {
+            "fio": "sggkh://ovyl.sfnrmyriw.xm/orev/yzooyzi_66629.n6f1",
+            "ozyvo": "SD [IOS]"
+        }
+    ],
+    "hkligIxlm": "sggkh://r.rntfi.xln/lJuBO44.kmt",
+    "vevmgNznv": "Eivwrerhrv",
+    "gvzn8": {
+        "oltl": "sggkh://hho.thgzgrx.xln/lmvylc/nvwrz/hkligh/oltlh/a3E3BNmk1PqBXguArZ3bNt_03c03.kmt",
+        "mznv": "NEC"
+    },
+    "gvzn7": {
+        "oltl": "sggkh://hho.thgzgrx.xln/lmvylc/nvwrz/hkligh/oltlh/vupzF8E6Z0UyMSO9VC0WJQ_03c03.kmt",
+        "mznv": "Gilmrmtvm"
+    },
+    "vevmgDzgv": "7975-88-96",
+    "vevmgTrnv": "79:69",
+    "wfizgrlm": "7.94",
+    "xlfmgwldmIw": "xlfmgwldm-2u13x9x1-7x3y-58u5-0486-0x7z1xx3u8z6"
 }
 ];
 // END EVENT DATA

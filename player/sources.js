38e6225a9c98b7778afca0652488b63eca5aad62
JensSudibyo <135489722+JensSudibyo@users.noreply.github.com>
2025-04-14 00:26:17 +0800

    Create sources.js

---
+++ player/sources.js
@@ -0,0 +1,7 @@
+// sources.js
+export default {
+  // Awal Data
+  'W17BcXQCga': 'https://live2.jiatianxiazhuangshi.com/live/57436635_e1ff9fb3cc946bcb4f302770ce291357_nsd.m3u8',
+  '5srQ75vSwK': 'https://sport.inplaynet.tech/livestream.html?src=aHR0cHM6Ly9zbWQuc3BvcnRwbGF5ZXIuaW8vZW1iZWQucGhwLz9tYXRjaF9pZD0xMjUyODUyNyZzaz0xYzlhYzAxJnR5cGU9cw==',
+  // Akhir Data
+};

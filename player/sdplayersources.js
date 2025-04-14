c7bf4ca815b6152b62c2305fada3d3195d867c0a
JensSudibyo <135489722+JensSudibyo@users.noreply.github.com>
2025-04-14 18:37:40 +0800

    Update M3U8 data

--- player/sdplayersources.js
+++ player/sdplayersources.js
@@ -11,6 +11,6 @@ export default {
 'tennistv2': 'https://d3qg0hadt0dkov.cloudfront.net/v1/master/3722c60a815c199d9c0ef36c5b73da68a62b09d1/cc-3sfgyrfz9zduo-prod/amgtennisus/playlist.m3u8',
 // Akhir Data TV M3U8
 // Awal Data Server SD
-'6lMFEthWlb': 'https://op-group1-swiftservehd-1.dens.tv/h/h207/index.m3u8',
+'6lMFEthWlb': 'https://sport.inplaynet.tech/livestream.html?src=aHR0cHM6Ly9zbWQuc3BvcnRwbGF5ZXIuaW8vZW1iZWQucGhwLz9tYXRjaF9pZD0xMjU1MDEzOCZzaz0xYzlhYzAxJnR5cGU9cw==',
 // Akhir Data Server SD
 };

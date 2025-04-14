2b240aeeed501633d68f7da4d5a99fb720684946
JensSudibyo <135489722+JensSudibyo@users.noreply.github.com>
2025-04-14 16:11:46 +0800

    Update sdplayersources.js

--- player/sdplayersources.js
+++ player/sdplayersources.js
@@ -1,13 +1,13 @@
 export default {
-  // Awal Data
-          'btv': 'https://b1news.beritasatumedia.com/Beritasatu/B1News_manifest.m3u8',
-          'sctv': 'https://op-group1-swiftservehd-1.dens.tv/h/h217/index.m3u8',
-          'moji': 'https://op-group1-swiftservehd-1.dens.tv/h/h207/index.m3u8',
-          'indosiar': 'https://op-group1-swiftservehd-1.dens.tv/h/h235/index.m3u8',
-          'tvrisports': 'https://op-group1-swiftservesd-1.dens.tv/s/s107/index.m3u8',  
-          'tvriworld': 'https://ott-balancer.tvri.go.id/live/eds/TVRIWorld/hls/TVRIWorld.m3u8', 
-          'kplus': 'https://op-group1-swiftservehd-1.dens.tv/h/h08/01.m3u8',  
-          'tennistv': 'https://cdn-uw2-prod.tsv2.amagi.tv/linear/amg01444-tennischannelth-tennischnlintl-lggb/playlist.m3u8',
-          'tennistv2': 'https://d3qg0hadt0dkov.cloudfront.net/v1/master/3722c60a815c199d9c0ef36c5b73da68a62b09d1/cc-3sfgyrfz9zduo-prod/amgtennisus/playlist.m3u8',
-  // Akhir Data
+// Awal Data
+'btv': 'https://b1news.beritasatumedia.com/Beritasatu/B1News_manifest.m3u8',
+'sctv': 'https://op-group1-swiftservehd-1.dens.tv/h/h217/index.m3u8',
+'moji': 'https://op-group1-swiftservehd-1.dens.tv/h/h207/index.m3u8',
+'indosiar': 'https://op-group1-swiftservehd-1.dens.tv/h/h235/index.m3u8',
+'tvrisports': 'https://op-group1-swiftservesd-1.dens.tv/s/s107/index.m3u8',  
+'tvriworld': 'https://ott-balancer.tvri.go.id/live/eds/TVRIWorld/hls/TVRIWorld.m3u8', 
+'kplus': 'https://op-group1-swiftservehd-1.dens.tv/h/h08/01.m3u8',  
+'tennistv': 'https://cdn-uw2-prod.tsv2.amagi.tv/linear/amg01444-tennischannelth-tennischnlintl-lggb/playlist.m3u8',
+'tennistv2': 'https://d3qg0hadt0dkov.cloudfront.net/v1/master/3722c60a815c199d9c0ef36c5b73da68a62b09d1/cc-3sfgyrfz9zduo-prod/amgtennisus/playlist.m3u8',
+// Akhir Data
 };

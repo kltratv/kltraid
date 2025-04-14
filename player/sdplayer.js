69388985b4d2a3422bc0ee4504622daa29b865b8
JensSudibyo <135489722+JensSudibyo@users.noreply.github.com>
2025-04-14 15:21:30 +0800

    Update sdplayer.js

--- player/sdplayer.js
+++ player/sdplayer.js
@@ -1,4 +1,4 @@
-  import sources from './sources.js';
+  import sources from './sdplayersources.js';
   jwplayer.key = 'XSuP4qMl+9tK17QNb+4+th2Pm9AWgMO/cYH8CI0HGGr7bdjo';
 
   let playerInstance = null;

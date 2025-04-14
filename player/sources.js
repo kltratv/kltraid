13716c3d82840311b6d5417a2a1f46807e936f98
JensSudibyo <135489722+JensSudibyo@users.noreply.github.com>
2025-04-14 11:28:41 +0800

    Update sources.js

--- player/sources.js
+++ player/sources.js
@@ -1,7 +1,7 @@
 // sources.js
 export default {
   // Awal Data
-  'W17BcXQCga': 'https://live2.jiatianxiazhuangshi.com/live/57436635_e1ff9fb3cc946bcb4f302770ce291357_nsd.m3u8',
+  'W17BcXQCga': 'https://op-group1-swiftservehd-1.dens.tv/h/h217/index.m3u8',
   'ZIxRAUjuq8': 'https://sport.inplaynet.tech/livestream.html?src=aHR0cHM6Ly9zbWQuc3BvcnRwbGF5ZXIuaW8vZW1iZWQucGhwLz9tYXRjaF9pZD0xMjU0Njk1NCZzaz0xYzlhYzAxJnR5cGU9cw==',
   // Akhir Data
 };

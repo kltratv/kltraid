8489cb747a6c5e87a4c05f9368451d4c284768a3
JensSudibyo <135489722+JensSudibyo@users.noreply.github.com>
2025-04-14 16:14:38 +0800

    Update sdplayersources.js

--- player/sdplayersources.js
+++ player/sdplayersources.js
@@ -1,5 +1,5 @@
 export default {
-// Awal Data
+// Awal Data TV M3U8
 'btv': 'https://b1news.beritasatumedia.com/Beritasatu/B1News_manifest.m3u8',
 'sctv': 'https://op-group1-swiftservehd-1.dens.tv/h/h217/index.m3u8',
 'moji': 'https://op-group1-swiftservehd-1.dens.tv/h/h207/index.m3u8',
@@ -9,5 +9,7 @@ export default {
 'kplus': 'https://op-group1-swiftservehd-1.dens.tv/h/h08/01.m3u8',  
 'tennistv': 'https://cdn-uw2-prod.tsv2.amagi.tv/linear/amg01444-tennischannelth-tennischnlintl-lggb/playlist.m3u8',
 'tennistv2': 'https://d3qg0hadt0dkov.cloudfront.net/v1/master/3722c60a815c199d9c0ef36c5b73da68a62b09d1/cc-3sfgyrfz9zduo-prod/amgtennisus/playlist.m3u8',
-// Akhir Data
+// Akhir Data TV M3U8
+// Awal Data Server SD
+// Akhir Data Server SD
 };

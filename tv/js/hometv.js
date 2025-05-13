5813f08276417b3af1f5b243530b889902da21e4
JensSudibyo <135489722+JensSudibyo@users.noreply.github.com>
2025-05-13 14:55:50 +0800

    Update hometv.js

--- tv/js/hometv.js
+++ tv/js/hometv.js
@@ -4,7 +4,7 @@
 
     async function loadChannelsFromJSON() {
         try {
-            const res = await fetch('https://kltraid.pages.dev/json/channel.json');
+            const res = await fetch('https://content.elutuna.workers.dev/channel.json');
             if (!res.ok) throw new Error('Failed to load channel.json');
             const data = await res.json();
             const container = document.querySelector('#live-tv #content');
@@ -32,7 +32,7 @@
     }
 
     async function loadEventsFromJSON() {
-        const res = await fetch('https://kltraid.pages.dev/json/event.json');
+        const res = await fetch('https://content.elutuna.workers.dev/event.json');
         const data = await res.json();
         const container = document.querySelector('#live-event #content');
         container.innerHTML = ""; // Kosongkan kontainer lama

570c1d3850dec6ea71393ad6c8faf26455e6bacc
JensSudibyo <135489722+JensSudibyo@users.noreply.github.com>
2025-05-26 23:47:41 +0800

    Update event-data.js dari Google Sheets

--- json/event-data.js
+++ json/event-data.js
@@ -16,5 +16,25 @@
   }
 
   window.__EVENT_DATA__ =
-[]
+[
+  {
+    "id": "912533f8-c089-4fef-8e00-0d53ace2ba1a",
+    "duration": 2000,
+    "league": "2025 MotoGP British Grand Prix",
+    "icon": "https://kltraid.pages.dev/images/sportsicon/Balapan.png",
+    "team1": {
+      "name": "MotoGP",
+      "logo": "https://upload.wikimedia.org/wikipedia/commons/a/a0/Moto_Gp_logo.svg"
+    },
+    "team2": {
+      "name": "MotoGP",
+      "logo": "https://upload.wikimedia.org/wikipedia/commons/a/a0/Moto_Gp_logo.svg"
+    },
+    "kickoff_date": "2025-05-23",
+    "kickoff_time": "16:00",
+    "match_date": "2025-05-23",
+    "match_time": "16:00",
+    "firstRow": 4
+  }
+]
 })();

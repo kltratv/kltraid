e87e4a5a46f966c539bfc9ff7b579e052dccf93f
JensSudibyo <135489722+JensSudibyo@users.noreply.github.com>
2025-06-06 11:12:57 +0800

    Update hitstats.js

--- js/hitstats.js
+++ js/hitstats.js
@@ -7,5 +7,5 @@ _Hasync.push(['Histats.track_hits', '']);
      var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
      hs.src = ('//s10.histats.com/js15_as.js');
      (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
-})();
+})();</script>
 // Histats.com  END

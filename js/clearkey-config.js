f80f6695eef871b1d55f33fa890f985d0301e506
JensSudibyo <135489722+JensSudibyo@users.noreply.github.com>
2025-06-09 05:03:47 +0800

    Update clearkey-config.js

--- js/clearkey-config.js
+++ js/clearkey-config.js
@@ -1,14 +1,11 @@
 window.clearKeyMap = {
-  'sportstar1': { 
-    url: 'https://fta2-cdn-flr.visionplus.id/out/v1/89a6e4261cd7470f83e5869e90440cff/index.mpd', 
-    drm: { '39c4dc6704cf4ceea2fd4863b88d8a7d': '4e9d7954c2ff46759289da4fc9f018ea' }
-  },  
-  'sportstar2': { 
-    url: 'https://fta3-cdn-flr.visionplus.id/out/v1/d2c68a3dfb644808b416bd90dcc92d5f/index.mpd', 
-    drm: { '911e72adf36946afbdbb4f80782a8394': '08aec548a851ba64b7172ae7f05cb91c' }
-  },  
-  'soccerchannel': { 
-    url: 'https://fta4-cdn-flr.visionplus.id/out/v1/63c0da12bb4d48afbaf053f51dff2353/index.mpd', 
-    drm: { '4035323a7fe64767ab8f3345ed9b93be': '67377b8d429603f8bf30c161bda269e5' }
+  'soccerchannel': {
+    url: 'https://fta4-cdn-flr.visionplus.id/out/v1/63c0da12bb4d48afbaf053f51dff2353/index.mpd',
+    drm: (function () {
+      return {
+        [hexToBase64('4035323a7fe64767ab8f3345ed9b93be')]:
+        hexToBase64('67377b8d429603f8bf30c161bda269e5')
+      };
+    })()
   }
 };

5bec4ecbcb45c13728c65ef5ae15a16a79e1a301
JensSudibyo <135489722+JensSudibyo@users.noreply.github.com>
2025-06-09 20:52:01 +0800

    Update clearkey-config.js

--- js/clearkey-config.js
+++ js/clearkey-config.js
@@ -1,12 +1,8 @@
 window.clearKeyMap = {
-  'soccerchannel': (function () {
-    const keyIdHex = '4035323a7fe64767ab8f3345ed9b93be';
-    const keyHex = '67377b8d429603f8bf30c161bda269e5';
-    return {
-      url: 'https://fta4-cdn-flr.visionplus.id/out/v1/63c0da12bb4d48afbaf053f51dff2353/index.mpd',
-      drm: {
-        [hexToBase64(keyIdHex)]: hexToBase64(keyHex)
-      }
-    };
-  })()
+  'soccerchannel': {
+    url: 'https://fta4-cdn-flr.visionplus.id/out/v1/63c0da12bb4d48afbaf053f51dff2353/index.mpd',
+    drm: {
+      '4035323a7fe64767ab8f3345ed9b93be': '67377b8d429603f8bf30c161bda269e5'
+    }
+  }
 };

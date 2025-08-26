7da7c0974445f70c7a1fd7ec80518058b44dafb1
JensSudibyo <135489722+JensSudibyo@users.noreply.github.com>
2025-08-26 23:41:08 +0800

    Delete js/clearkey-config.js

--- js/clearkey-config.js
+++
@@ -1,8 +0,0 @@
-window.clearKeyMap = {
-  'soccerchannel': {
-    url: 'https://fta4-cdn-flr.visionplus.id/out/v1/63c0da12bb4d48afbaf053f51dff2353/index.mpd',
-    drm: {
-      '4035323a7fe64767ab8f3345ed9b93be': '67377b8d429603f8bf30c161bda269e5'
-    }
-  }
-};
